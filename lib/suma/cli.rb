4025f24768ec5f334256c22d514973b3145f9162
Ronald Tse <ronald.tse@ribose.com>
2025-03-21 19:43:23 +0800

    feat: refactor cli module

--- lib/suma/cli.rb
+++ lib/suma/cli.rb
@@ -4,22 +4,25 @@ require "thor"
 require_relative "thor_ext"
 
 module Suma
-  class Cli < Thor
-    extend ThorExt::Start
+  module Cli
+    # Core command class for handling CLI entrypoints
+    class Core < Thor
+      extend ThorExt::Start
 
-    desc "build METANORMA_SITE_MANIFEST",
-         "Build collection specified in site manifest (`metanorma*.yml`)"
-    def build(*args)
-      # If no arguments, add an empty array to ensure the default command is triggered
-      args = [] if args.empty?
-      require_relative "cli/build"
-      Build.start(args)
-    end
+      desc "build METANORMA_SITE_MANIFEST",
+           "Build collection specified in site manifest (`metanorma*.yml`)"
+      def build(*args)
+        # If no arguments, add an empty array to ensure the default command is triggered
+        args = [] if args.empty?
+        require_relative "cli/build"
+        Cli::Build.start(args)
+      end
 
-    desc "links SUBCOMMAND ...ARGS", "Manage EXPRESS links"
-    def links(*args)
-      require_relative "cli/links"
-      Links.start(args)
+      desc "links SUBCOMMAND ...ARGS", "Manage EXPRESS links"
+      def links(*args)
+        require_relative "cli/links"
+        Cli::Links.start(args)
+      end
     end
   end
 end
--- lib/suma/cli/build.rb
+++ lib/suma/cli/build.rb
@@ -4,56 +4,58 @@ require "thor"
 require_relative "../thor_ext"
 
 module Suma
-  # Build command for building collections
-  class Build < Thor
-    # Return true to exit with non-zero status on errors
-    def self.exit_on_failure?
-      true
-    end
-
-    default_command :default_build
-
-    desc "default_build METANORMA_SITE_MANIFEST",
-         "Build collection specified in site manifest (`metanorma*.yml`)"
-    option :compile, type: :boolean, default: true,
-                     desc: "Compile or skip compile of collection"
-    option :schemas_all_path, type: :string, aliases: "-s",
-                              desc: "Generate file that contains all schemas in the collection."
-
-    def default_build(metanorma_site_manifest)
-      # Lazy-load dependencies only when this command is actually used
-      require_relative "../processor"
-      require_relative "../utils"
-
-      unless File.exist?(metanorma_site_manifest)
-        raise Errno::ENOENT, "Specified Metanorma site manifest file " \
-                             "`#{metanorma_site_manifest}` not found."
+  module Cli
+    # Build command for building collections
+    class Build < Thor
+      # Return true to exit with non-zero status on errors
+      def self.exit_on_failure?
+        true
       end
 
-      # Allow errors to propagate
-      run(metanorma_site_manifest, options)
-    end
+      default_command :default_build
 
-    private
+      desc "default_build METANORMA_SITE_MANIFEST",
+           "Build collection specified in site manifest (`metanorma*.yml`)"
+      option :compile, type: :boolean, default: true,
+                       desc: "Compile or skip compile of collection"
+      option :schemas_all_path, type: :string, aliases: "-s",
+                                desc: "Generate file that contains all schemas in the collection."
 
-    def run(manifest, options)
-      # Set schemas_all_path to match metanorma_yaml_path
-      schemas_all_path = options[:schemas_all_path] ||
-        manifest.gsub("metanorma", "schemas")
+      def default_build(metanorma_site_manifest)
+        # Lazy-load dependencies only when this command is actually used
+        require_relative "../processor"
+        require_relative "../utils"
 
-      Processor.run(
-        metanorma_yaml_path: manifest,
-        schemas_all_path: schemas_all_path,
-        compile: options[:compile],
-        output_directory: "_site",
-      )
-    end
+        unless File.exist?(metanorma_site_manifest)
+          raise Errno::ENOENT, "Specified Metanorma site manifest file " \
+                               "`#{metanorma_site_manifest}` not found."
+        end
 
-    def log_error(error)
-      Utils.log "[ERROR] Error occurred during processing. See details below."
-      Utils.log error
-      Utils.log error.inspect
-      Utils.log error.backtrace.join("\n")
+        # Allow errors to propagate
+        run(metanorma_site_manifest, options)
+      end
+
+      private
+
+      def run(manifest, options)
+        # Set schemas_all_path to match metanorma_yaml_path
+        schemas_all_path = options[:schemas_all_path] ||
+          manifest.gsub("metanorma", "schemas")
+
+        Processor.run(
+          metanorma_yaml_path: manifest,
+          schemas_all_path: schemas_all_path,
+          compile: options[:compile],
+          output_directory: "_site",
+        )
+      end
+
+      def log_error(error)
+        Utils.log "[ERROR] Error occurred during processing. See details below."
+        Utils.log error
+        Utils.log error.inspect
+        Utils.log error.backtrace.join("\n")
+      end
     end
   end
 end
