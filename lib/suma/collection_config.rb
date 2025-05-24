53062db4e5f365897871f37e583c46a897b23869
Nick Nicholas <opoudjis@gmail.com>
2025-05-24 14:15:36 +1000

    do not manipulate YAML in suma: https://github.com/metanorma/metanorma-iso/issues/1217

--- lib/suma/collection_config.rb
+++ lib/suma/collection_config.rb
@@ -11,19 +11,8 @@ module Suma
   class CollectionConfig < Metanorma::Collection::Config::Config
     attribute :manifest, ::Suma::CollectionManifest
 
-    def self.preprocess_yaml(file)
-        #yaml = YAML.safe_load(file)
-        #flavor = yaml["directives"]&.detect { |x| x.is_a?(Hash) && x.has_key?("flavor") }
-          #&.dig("flavor")&.upcase or return file
-        #yaml["bibdata"] or return file
-        #yaml["bibdata"]["ext"] ||= {}
-        #yaml["bibdata"]["ext"]["flavor"] ||= flavor
-        #yaml.to_yaml
-      file
-      end
-
     def self.from_file(path)
-      from_yaml(preprocess_yaml(File.read(path)))
+      from_yaml(File.read(path))
     end
 
     def to_file(path)
