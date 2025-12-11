e2a9a3f743bd11647550618d619fb6ea23178909
Ronald Tse <ronald.tse@ribose.com>
2025-12-11 11:48:13 +0800

    feat: remove `validate ascii` command, moved to lutaml/expressir#278, fixes #75

--- lib/suma/cli/validate.rb
+++ lib/suma/cli/validate.rb
@@ -15,20 +15,6 @@ module Suma
         links = Cli::ValidateLinks.new
         links.extract_and_validate(*args)
       end
-
-      desc "ascii EXPRESS_FILE_PATH",
-           "Validate EXPRESS files for ASCII-only content"
-      option :recursive, type: :boolean, default: false, aliases: "-r",
-                         desc: "Validate EXPRESS files under the specified path recursively"
-      option :yaml, type: :boolean, default: false, aliases: "-y",
-                    desc: "Output results in YAML format"
-      def ascii(express_file_path)
-        require_relative "validate_ascii"
-
-        validator = Cli::ValidateAscii.new
-        validator.options = options
-        validator.validate_ascii(express_file_path)
-      end
     end
   end
 end
