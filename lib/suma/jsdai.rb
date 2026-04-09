cbdefe02e93fbbdae8d7b2b8f22838743e7c8bbd
Ronald Tse <ronald.tse@ribose.com>
2026-04-09 15:44:53 +0800

    feat: update lutaml-model to 0.8.0

--- lib/suma/jsdai.rb
+++ lib/suma/jsdai.rb
@@ -4,9 +4,3 @@ module Suma
 end
 
 require_relative "jsdai/figure"
-
-# Configure XML adapter to Nokogiri because Ox goes into a "stack level too
-# deep" error, for unknown reasons
-Lutaml::Model::Config.configure do |config|
-  config.xml_adapter_type = :nokogiri
-end
